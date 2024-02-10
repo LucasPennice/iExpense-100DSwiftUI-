a8a1c66160f77c37f7422c3f193d29d5bbdd27b7
Lucas Pennice <lucaspennice@gmail.com>
2024-02-10 12:55:04 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Lucas Pennice on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
